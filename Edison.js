6cf9c1783e81dc5483dae5debc0137e0be34a419
edisonchan97 <edisonzhuozhou@163.com>
2018-03-06 16:05:23 +0800

    setCookie

--- Edison.js
+++ Edison.js
@@ -1,1 +1,17 @@
-
+/*
+操作cookie
+1.key
+2.value
+3.expires
+4.domain
+5.path
+*/
+const Cookie = {};
+Cookie.cookie = function(){
+      return document.cookie;
+}();
+Cookie.setCookie = function(c_name,value,expiredays){
+      var exdate=new Date()
+      exdate.setDate(exdate.getDate()+expiredays)
+      document.cookie=c_name+ "=" +escape(value)+((expiredays==null) ? "" : "; expires="+exdate.toGMTString());
+}
